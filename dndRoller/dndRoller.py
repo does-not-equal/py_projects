1f5aa968ea6f004eb7b18db8ff0b7dbe2b7d5e0c
does-not-equal <acornheroics@gmail.com>
2023-08-30 16:39:22 -0700

    restart project, updated notes

--- dndRoller/dndRoller.py
+++ dndRoller/dndRoller.py
@@ -18,95 +18,42 @@ def feats_subtext():
     
 def stats_subtext():
     print('These are your stats')
-    
-def char_races():
-    with open(char_races) as char_races_f:
-        char_races_f = char_races_f.read()
-        print(char_races)
-        
-def char_classes():
-    char_classes_file = open(charClasses)
-    contents = char_classes_file.read()
-    print(contents)
-    char_classes_file.close()
 
-def char_feats():
-    char_feats_file = open(charFeats)
-    contents = char_feats_file.read()
-    print (contents)
-    char_feats_file.close()
+def char_read():
+    player_input = input()
+    if player_input == 'h':
+        with open(char_races) as char_races_h:
+            char_races_h = char_races_h.read()
+            print(char_races)
+    elif player_input == 'he':
+        with open(char_races) as char_races_he:
+            char_races_he = char_races_he.read()
+            print(char_races)
+    elif player_input == 'o':
+        with open(char_races) as char_races_o:
+            char_races_o = char_races_o.read()
+            print(char_races)
+    elif player_input == 'g':
+        with open(char_races) as char_races_g:
+            char_races_g = char_races_g.read()
+            print(char_races)
+    elif player_input == 'ho':
+        with open(char_races) as char_races_ho:
+            char_races_ho = char_races_ho.read()
+            print(char_races)
+    elif player_input == 'f':
+        with open(char_races) as char_races_f:
+            char_races_f = char_races_f.read()
+            print(char_races)
 
-def char_stats():
-    char_stats_file = open(charStats)
-    contents = char_stats_file.read()
-    print (contents)
-    char_stats_file.close()
 
 def player_race_loop():
     while True:
-        print('Please choose a class or type (q) to exit:', c_races())
+        print('Please choose a class or type (q) to exit:')
         player_input = input()
         if player_input == 'q':
             sys.exit()
-        if player_input == 'h':
-            print('You chose human and receive a +1 to all stats.')
-            race_subtext()
-            player_input = input()
-            if player_input == 'y':
-                player_class_loop()
-            elif player_input == 'n':
-                player_race_loop()
-            elif player_input != 'y' or 'n':
-                sys.exit()
-        elif player_input == 'he':
-            print('You chose half elf and receive a +1 perception and +1 agility.')
-            race_subtext()
-            player_input = input()
-            if player_input == 'y':
-                player_class_loop()
-            elif player_input == 'n':
-                player_race_loop()
-            elif player_input != 'y' or 'n':
-                sys.exit()
-        elif player_input == 'o':
-            print('You chose orc and receive a +1 endurance, +1 strength, and -1 charisma.')
-            race_subtext()
-            player_input = input()
-            if player_input == 'y':
-                player_class_loop()
-            elif player_input == 'n':
-                player_race_loop()
-            elif player_input != 'y' or 'n':
-                sys.exit()
-        elif player_input == 'g':
-            print('You chose gnome and receive a -1 strength, +1 perception, and +1 intelligence.')
-            race_subtext()
-            player_input = input()
-            if player_input == 'y':
-                player_class_loop()
-            elif player_input == 'n':
-                player_race_loop()
-            elif player_input != 'y' or 'n':
-                sys.exit()
-        elif player_input == 'ho':
-            print('You chose hobgoblin and receive a +1 endurance, +2 strength, -2 charisma, and -1 intelligence.')
-            race_subtext()
-            player_input = input()
-            if player_input == 'y':
-                player_class_loop()
-            elif player_input == 'n':
-                player_race_loop()
-            elif player_input != 'y' or 'n':
-                sys.exit()
-        elif player_input == 'f':
-            print('You chose fae and receive a -2 strength, -1 endurance, +2 all other stats.')
-            race_subtext()
-            player_input = input()
-            if player_input == 'y':
-                player_class_loop()
-            elif player_input == 'n':
-                player_race_loop()
-            elif player_input != 'y' or 'n':
-                sys.exit()
+        elif player_input == 'h' or 'he' or 'o' or 'g' or 'ho' or 'f':
+            char_read()
             
 player_race_loop()
