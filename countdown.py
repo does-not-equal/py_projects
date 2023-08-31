5987a57ba962cfff0195da03c82bf3ec5291c373
does-not-equal <acornheroics@gmail.com>
2023-08-31 03:42:45 -0700

    added new projects

---
+++ countdown.py
@@ -0,0 +1,10 @@
+# countdown script
+import time, subprocess
+
+timeLeft = 60
+while timeLeft > 0:
+    print(timeLeft, end='')
+    time.sleep(1)
+    timeLeft = timeLeft - 1
+    
+subprocess.Popen(['start', 'alarm.wav'], shell=True)
---
+++ pyGentlyExplained/helloWorld.py
@@ -0,0 +1,4 @@
+print ('Hello world!')
+print ('What is your name?')
+user_name = input()
+print ('Hello, ' + user_name)
