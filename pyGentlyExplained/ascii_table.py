515b8e860331b2da95af09213fad8771e13dcafa
does-not-equal <acornheroics@gmail.com>
2023-09-01 20:59:14 -0700

    added ascii table project

---
+++ pyGentlyExplained/ascii_table.py
@@ -0,0 +1,8 @@
+# some help was needed but I fully understood what
+# was going on at the end!
+
+def print_ascii_table():
+    for i in range (32, 126 + 1):
+        print (i, chr(i))
+
+print_ascii_table()
