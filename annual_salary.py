fb5a9784851ea1c1b592b5e83f2dfc0f4eeafc1b
does-not-equal <does-not-equal@tuta.io>
2023-09-18 17:55:32 -0700

    added annual salary project

---
+++ annual_salary.py
@@ -0,0 +1,8 @@
+def annual_salary():
+    total_hours = int(input("Please input your weekly hours: "))
+    hourly = int(input("Please input your hourly wage: "))
+    weekly = total_hours * hourly
+    salary = weekly * 52
+    return print("$" + str(salary))
+
+annual_salary()
